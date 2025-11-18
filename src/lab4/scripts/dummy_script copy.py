e44870335943273f8059fb2f3e3d65586111795e
phattanaratjeedjeen-sudo <phattanarat.jeedjeen@gmail.com>
2025-11-18 15:47:26 +0700

    edit controller interfaces

--- src/lab4/scripts/dummy_script copy.py
+++ src/lab4/scripts/dummy_script copy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from controller_interfaces.dummy_module import dummy_function, dummy_var
+from lab4.dummy_module import dummy_function, dummy_var
 import rclpy
 from rclpy.node import Node
 
